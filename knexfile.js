528866c3ab9fb44e3c3063dbaa2508f7e84c1129
colinbinion <colin.binion@me.com>
2016-09-01 19:17:16 -0600

    ready for heroku

--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,13 @@
+require('dotenv').load();
+
+
 module.exports = {
+
+
 
   production: {
     client: 'postgresql',
-    connection: 'postgres://localhost/knex-heroku',
+    connection: process.env.DATABASE_URL,
     pool : {
       min: 2,
       max: 10
